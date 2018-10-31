40aa2c8dcdf3c3bcce5452b1276354be288c3766
Juliandb1708 <38686278+Juliandb1708@users.noreply.github.com>
2018-10-31 13:14:26 +0100

    Design class diagram Iteratie 1 etc.

--- iteratie_1/uitvoeren-quiz/src/main/java/nl/han/ica/ooad/KortAntwoordvraag.java
+++ iteratie_1/uitvoeren-quiz/src/main/java/nl/han/ica/ooad/KortAntwoordvraag.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class KortAntwoordvraag extends Vraag {
     private List<Antwoord> mogelijkeAntwoorden;
+
     protected KortAntwoordvraag(int vraagnummer, String vraag, int punt, List<Antwoord> mogelijkeAntwoorden) {
         super(vraagnummer, vraag, punt);
         this.mogelijkeAntwoorden = mogelijkeAntwoorden;
--- iteratie_1/uitvoeren-quiz/src/main/java/nl/han/ica/ooad/MeerkeuzeVraag.java
+++ iteratie_1/uitvoeren-quiz/src/main/java/nl/han/ica/ooad/MeerkeuzeVraag.java
@@ -3,6 +3,7 @@ package nl.han.ica.ooad;
 public class MeerkeuzeVraag extends Vraag {
     private String [] opties;
     private Antwoord antwoord;
+
     protected MeerkeuzeVraag(int vraagnummer, String vraag, int punt, Antwoord antwoord, String[] opties) {
         super(vraagnummer, vraag, punt, antwoord);
         this.opties = opties;
